c68ec1c152958c89d9270e28e81cc18e31bb5d62
shxxll <breian157@gmail.com>
2024-06-18 23:55:23 +0800

    earl

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,7 @@ export default function LoginScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello</Text>
+      <Text style={styles.text}>Hello Earl</Text>
       <TextInput
         style={styles.input}
         placeholder="Email"
